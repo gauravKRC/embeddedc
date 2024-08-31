7f21aa2381244eeeee2bfe3daddc7730c90ed67f
gaurav <payme4work17461@gmail.com>
2024-08-31 18:50:05 +0530

    3

---
+++ 3.c
@@ -0,0 +1,15 @@
+/**
+* Question:Get a number from user and multiply 3 to that number and print the
+result.
+Example:Input: 45 Output 135. Input: 1200 Output: 3600
+    */
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=3*x;
+    printf("Result is:%d\n",y);
+return 0;
+}
