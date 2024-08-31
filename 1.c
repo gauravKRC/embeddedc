c9515e4e65261236325620359c9ee54d5ef1ff83
gaurav <payme4work17461@gmail.com>
2024-08-31 09:40:03 +0530

    1

---
+++ 1.c
@@ -0,0 +1,16 @@
+/***
+* Question:Get a number from user and add 2 to that number and print the result.
+  Example:Input :45 Output 47. Input:56789 Output:56791
+    */
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x+2;
+    printf("Result is:%d\n",y);
+return 0;
+}
+
+
