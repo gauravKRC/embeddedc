a38c7561b638c1a5be42cc51d030ab2b3c1fb91f
gaurav <payme4work17461@gmail.com>
2024-08-31 20:09:43 +0530

    4

---
+++ 4.c
@@ -0,0 +1,14 @@
+/**
+* Question:Get a number from user and divide by the number by 6 and print the
+quotient.
+Example:Input: 45 Output 7. Input: 143 Output: 23
+*/
+#include <stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    y=x/6;
+    printf("Result is:%d\n",y);
+return 0;
+}
